e98f92eb5d13266623af3222fc6f15ad2a233bd8
unconv <unconventionalcoding@gmail.com>
2024-05-31 13:02:19 +0300

    bumped version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_requirements():
 
 setup(
     name='captacity',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=get_requirements(),
     package_data={
