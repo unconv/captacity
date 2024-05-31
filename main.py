08085a18234f8fc452b8ae77e6d731011c74f1c2
unconv <unconventionalcoding@gmail.com>
2024-05-31 07:52:53 +0300

    added word highlighting

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 import sys
 import os
 
-from text_drawer import get_text_size, create_text
+from text_drawer import get_text_size_ex, create_text_ex, blur_text_clip, Word
 import segment_parser
 
 video_file = sys.argv[1]
@@ -23,52 +23,63 @@ stroke_width = 3
 stroke_color = "black"
 font_size = 130
 font_color = "yellow"
+word_highlight_color = "red"
+highlight_current_word = True
 padding = 50
 position = ("center", "center")
-shadow_strength = 1
+shadow_strength = 1.0
 shadow_blur = 0.1
 
 def fits_frame(frame_width):
     def fit_function(text):
-        return len(calculate_lines(text, frame_width)["lines"]) <= 1
+        return len(calculate_lines(text, frame_width)["lines"]) <= 2
     return fit_function
 
 def calculate_lines(text, frame_width):
-    lines_to_write = []
+    lines = []
 
     line_to_draw = None
     line = ""
     words = text.split()
     word_index = 0
+    total_height = 0
     while word_index < len(words):
         word = words[word_index]
         line += word + " "
-        text_size = get_text_size(line.strip(), font_size, font, stroke_width)
+        text_size = get_text_size_ex(line.strip(), font_size, font, stroke_width)
         text_width = text_size[0]
         line_height = text_size[1]
 
         if text_width > frame_width:
             if line_to_draw:
-                lines_to_write.append(line_to_draw)
+                lines.append(line_to_draw)
+                total_height += line_height
                 line_to_draw = None
             line = ""
         else:
-            line_to_draw = line.strip()
+            line_to_draw = {
+                "text": line.strip(),
+                "height": line_height,
+            }
             word_index += 1
 
     if line_to_draw:
-        lines_to_write.append(line_to_draw)
+        lines.append(line_to_draw)
+        total_height += line_height
 
     return {
-        "lines": lines_to_write,
-        "height": line_height,
+        "lines": lines,
+        "height": total_height,
     }
 
 def ffmpeg(command):
     return subprocess.run(command, capture_output=True)
 
-def create_shadow(text, font_size, font, caption, blur_radius: float, opacity=1):
-    shadow = create_text(text, font_size, "black", font, blur_radius=int(font_size*blur_radius), opacity=opacity)
+def create_shadow(text, font_size, font, caption, position, blur_radius: float, opacity=1):
+    shadow = create_text_ex(text, font_size, "black", font, opacity=opacity)
+
+    shadow = blur_text_clip(shadow, int(font_size*blur_radius))
+
     shadow = shadow.set_start(caption["start"])
     shadow = shadow.set_duration(caption["end"] - caption["start"])
     shadow = shadow.set_position(position)
@@ -103,27 +114,58 @@ def main():
     captions = segment_parser.parse(segments, fits_frame(text_bbox_width))
 
     for caption in captions:
-        line_data = calculate_lines(caption["text"], text_bbox_width)
+        captions_to_draw = []
+        if highlight_current_word:
+            for i, word in enumerate(caption["words"]):
+                if i+1 < len(caption["words"]):
+                    end = caption["words"][i+1]["start"]
+                else:
+                    end = word["end"]
 
-        lines = "\n".join(line_data["lines"])
+                captions_to_draw.append({
+                    "text": caption["text"],
+                    "start": word["start"],
+                    "end": end,
+                })
+        else:
+            captions_to_draw.append(caption)
 
-        # Create shadow
-        shadow_left = shadow_strength
-        while shadow_left >= 1:
-            shadow_left -= 1
-            shadow = create_shadow(lines, font_size, font, caption, shadow_blur, opacity=1)
-            clips.append(shadow)
+        for current_index, caption in enumerate(captions_to_draw):
+            line_data = calculate_lines(caption["text"], text_bbox_width)
 
-        if shadow_left > 0:
-            shadow = create_shadow(lines, font_size, font, caption, shadow_blur, opacity=shadow_left)
-            clips.append(shadow)
+            text_y_offset = video.h // 2 - line_data["height"] // 2
+            index = 0
+            for line in line_data["lines"]:
+                pos = ("center", text_y_offset)
 
-        # Create text
-        text = create_text(lines, font_size, font_color, font, stroke_color=stroke_color, stroke_width=stroke_width)
-        text = text.set_start(caption["start"])
-        text = text.set_duration(caption["end"] - caption["start"])
-        text = text.set_position(position)
-        clips.append(text)
+                words = line["text"].split()
+                word_list = []
+                for w in words:
+                    word_obj = Word(w)
+                    if highlight_current_word and index == current_index:
+                        word_obj.set_color(word_highlight_color)
+                    index += 1
+                    word_list.append(word_obj)
+
+                # Create shadow
+                shadow_left = shadow_strength
+                while shadow_left >= 1:
+                    shadow_left -= 1
+                    shadow = create_shadow(word_list, font_size, font, caption, pos, shadow_blur, opacity=1)
+                    clips.append(shadow)
+
+                if shadow_left > 0:
+                    shadow = create_shadow(word_list, font_size, font, caption, pos, shadow_blur, opacity=shadow_left)
+                    clips.append(shadow)
+
+                # Create text
+                text = create_text_ex(word_list, font_size, font_color, font, stroke_color=stroke_color, stroke_width=stroke_width)
+                text = text.set_start(caption["start"])
+                text = text.set_duration(caption["end"] - caption["start"])
+                text = text.set_position(pos)
+                clips.append(text)
+
+                text_y_offset += line["height"]
 
     video_with_text = CompositeVideoClip(clips)
 
--- text_drawer.py
+++ text_drawer.py
@@ -1,7 +1,40 @@
-from moviepy.editor import TextClip, ImageClip, VideoClip
-from PIL import Image, ImageFilter
+from moviepy.editor import TextClip, ImageClip, VideoClip, CompositeVideoClip
+from PIL import Image, ImageFilter, ImageFont
 import numpy
 import tempfile
+
+class Character:
+    def __init__(self, text, color=None):
+        self.text = text
+        self.color = color
+
+    def set_color(self, color):
+        self.color = color
+
+class Word:
+    def __init__(self, word, color=None):
+        self.word = word
+        self.color = color
+        self.characters = []
+
+        for char in word:
+            self.characters.append(Character(char, color))
+
+    def set_color(self, color):
+        self.color = color
+        for char in self.characters:
+            char.set_color(color)
+
+class TextClipEx(TextClip):
+    def __init__(self, txt=None, filename=None, size=None, color='black',
+                 bg_color='transparent', fontsize=None, font='Courier',
+                 stroke_color=None, stroke_width=1, method='label',
+                 kerning=None, align='center', interline=None,
+                 tempfilename=None, temptxt=None,
+                 transparent=True, remove_temp=True,
+                 print_cmd=False):
+        super().__init__(txt, filename, size, color, bg_color, fontsize, font, stroke_color, stroke_width, method, kerning, align, interline, tempfilename, temptxt, transparent, remove_temp, print_cmd)
+        self.text = txt
 
 def moviepy_to_pillow(clip) -> Image:
     temp_file = tempfile.NamedTemporaryFile(suffix=".png").name
@@ -10,32 +43,89 @@ def moviepy_to_pillow(clip) -> Image:
     return image
 
 def get_text_size(text, fontsize, font, stroke_width):
-    text_clip = TextClip(text, fontsize=fontsize, color="white", font=font, stroke_width=stroke_width)
+    text_clip = create_text(text, fontsize=fontsize, color="white", font=font, stroke_width=stroke_width)
+    return text_clip.size
+
+def get_text_size_ex(text, fontsize, font, stroke_width):
+    text_clip = create_text_ex(text, fontsize=fontsize, color="white", font=font, stroke_width=stroke_width)
+    return text_clip.size
+
+def blur_text_clip(text_clip, blur_radius: int) -> VideoClip:
+    # Convert TextClip to a PIL image
     pil_img = moviepy_to_pillow(text_clip)
-    return pil_img.size
+
+    # Offset blur to make it centered
+    offset = int(blur_radius * 0.6)
+
+    # Add empty space around text for blur
+    pil_img_padded = Image.new("RGBA", (pil_img.width + blur_radius * 3, pil_img.height + blur_radius * 3))
+    pil_img_padded.paste(moviepy_to_pillow(text_clip), (blur_radius+offset, blur_radius+offset))
+
+    # Create a blurred version of the text
+    pil_img_padded = pil_img_padded.filter(
+        ImageFilter.GaussianBlur(radius=blur_radius)
+    )
+
+    text_clip = ImageClip(numpy.array(pil_img_padded))
+    text_clip = text_clip.set_duration(text_clip.duration)
+
+    return text_clip
 
 def create_text(text, fontsize, color, font, bg_color='transparent', blur_radius: int=0, opacity=1, stroke_color=None, stroke_width=1, kerning=0) -> VideoClip:
-    text_clip = TextClip(text, fontsize=fontsize, color=color, bg_color=bg_color, font=font, stroke_color=stroke_color, stroke_width=stroke_width, kerning=kerning)
+    text_clip = TextClipEx(text, fontsize=fontsize, color=color, bg_color=bg_color, font=font, stroke_color=stroke_color, stroke_width=stroke_width, kerning=kerning)
 
     text_clip = text_clip.set_opacity(opacity)
 
     if blur_radius:
-        # Convert TextClip to a PIL image
-        pil_img = moviepy_to_pillow(text_clip)
-
-        # Offset blur to make it centered
-        offset = int(blur_radius * 0.6)
-
-        # Add empty space around text for blur
-        pil_img_padded = Image.new("RGBA", (pil_img.width + blur_radius * 3, pil_img.height + blur_radius * 3))
-        pil_img_padded.paste(moviepy_to_pillow(text_clip), (blur_radius+offset, blur_radius+offset))
-
-        # Create a blurred version of the text
-        pil_img_padded = pil_img_padded.filter(
-            ImageFilter.GaussianBlur(radius=blur_radius)
-        )
-
-        text_clip = ImageClip(numpy.array(pil_img_padded))
-        text_clip = text_clip.set_duration(text_clip.duration)
+        text_clip = blur_text_clip(text_clip, blur_radius)
 
     return text_clip
+
+def create_text_chars(text: list[Word] | list[Character], fontsize, color, font, bg_color='transparent', blur_radius: int=0, opacity=1, stroke_color=None, stroke_width=1, add_space_between_words=True) -> list[VideoClip]:
+    # Create a clip for each character
+    clips = []
+    for i, item in enumerate(text):
+        if isinstance(item, Word):
+            chars = item.characters
+            if add_space_between_words and i < len(text) - 1:
+                chars.append(Character(" ", item.color))
+        else:
+            chars = [item]
+
+        for char in chars:
+            clip = create_text(char.text, fontsize, char.color or color, font, bg_color, blur_radius, opacity, stroke_color, stroke_width)
+            clips.append(clip)
+
+    return clips
+
+def create_composite_text(text_clips: list[VideoClip], font, font_size) -> CompositeVideoClip:
+    clips = []
+
+    font = ImageFont.truetype(font, font_size)
+    scale_factor = 3.012 # factor to convert Pillow to MoviePy width
+
+    full_width = 0
+    for clip in text_clips[:-1]:
+        width = font.getlength(clip.text) * scale_factor
+        full_width += width
+
+    full_width += text_clips[-1].size[0]
+    offset_x = 0
+
+    for clip in text_clips:
+        clip.size = (int(full_width), clip.size[1])
+        clip = clip.set_position((int(offset_x), 0))
+        width = font.getlength(clip.text)
+        offset_x += width * scale_factor
+        clips.append(clip)
+
+    return CompositeVideoClip(clips)
+
+def str_to_charlist(text: str) -> list[Character]:
+    return [Character(char) for char in text]
+
+def create_text_ex(text: list[Word] | list[Character] | str, fontsize, color, font, bg_color='transparent', blur_radius: int=0, opacity=1, stroke_color=None, stroke_width=1, kerning=0) -> CompositeVideoClip:
+    if isinstance(text, str):
+        text = str_to_charlist(text)
+    text_clips = create_text_chars(text, fontsize, color, font, bg_color, blur_radius, opacity, stroke_color, stroke_width)
+    return create_composite_text(text_clips, font, fontsize // 3)
